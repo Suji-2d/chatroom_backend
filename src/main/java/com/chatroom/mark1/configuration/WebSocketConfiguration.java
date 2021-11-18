bc36a6b00f56dd940518f6b849bf561231a9ad08
Suji-2d <khsujithkannan@gmail.com>
2021-11-18 16:11:31 +0530

    api first commit

---
+++ src/main/java/com/chatroom/mark1/configuration/WebSocketConfiguration.java
@@ -0,0 +1,26 @@
+package com.chatroom.mark1.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.chatroom.mark1.handler.ChatWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+
+	private static final String CHAT_ENDPOINT = "/chat";
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(getChatWebSocketHandler(), CHAT_ENDPOINT).setAllowedOriginPatterns("*");
+	}
+
+	@Bean
+	public WebSocketHandler getChatWebSocketHandler() {
+		return new ChatWebSocketHandler();
+	}
+}
---
+++ src/main/java/com/chatroom/mark1/handler/ChatWebSocketHandler.java
@@ -0,0 +1,39 @@
+package com.chatroom.mark1.handler;
+
+import java.net.http.WebSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class ChatWebSocketHandler extends TextWebSocketHandler{
+
+
+	private final List<WebSocketSession> webSocketSessions = new ArrayList<>();
+	
+	@Override
+	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+		webSocketSessions.add(session);
+		System.out.println("connection established : "+session.toString());
+	}
+	
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		for(WebSocketSession webSocketSession : webSocketSessions) {
+			webSocketSession.sendMessage(message);
+			System.out.println(message.toString());
+		}
+	}
+	
+	@Override
+	public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+		System.out.println(session.toString());
+		webSocketSessions.remove(session);
+		
+	}
+}
